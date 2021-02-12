f50a5d96b3250382fb43789c5dd5403f210bf43e
Azaze1l <56613711+Azaze1l@users.noreply.github.com>
2021-02-12 23:37:56 +0300

    2 task
    second task've been complited

--- script.py
+++ script.py
@@ -1,6 +1,6 @@
 from aiohttp import web
 import json
-
+from math import ceil
 """
 TODO:
 Реализованный сервер должен предоставлять REST API сервис со следующими методами:
@@ -32,12 +32,6 @@ def make_a_city_dict(city):
     return city_dict
 
 
-async def handle(request):
-    response_obj = {'status': 'success'}
-    print(request)
-    return web.Response(text=json.dumps(response_obj), status=200)
-
-
 async def info_by_id(request):
     """
     Метод принимает идентификатор geonameid и возвращает информацию о городе.
@@ -59,19 +53,57 @@ async def info_by_id(request):
 
         message = make_a_city_dict(cities_list[right_b])
         response_obj = {'status': 'success', 'message': message}
-        return web.Response(text=json.dumps(response_obj), status=200)
+        return web.Response(text=json.dumps(response_obj, ensure_ascii=False), status=200)
     except Exception as e:
         response_obj = {'status': 'failed', 'message': 'Parameter id is not define: {0}'.format(str(e))}
         return web.Response(text=json.dumps(response_obj), status=500)
 
 
 async def cities_page(request):
+    """
+    Метод принимает страницу и количество отображаемых на странице городов и возвращает список городов с их информацией.
+    """
+    try:
+        page = int(request.query['page'])
+        cnt_cities_on_page = int(request.query['count'])
+
+        all_cities = 201177
+        if cnt_cities_on_page > all_cities or cnt_cities_on_page == 0:
+            response_obj = {'status': 'failed', 'message': "This page doesn't exist:"
+                                                           " something wrong with count of cities on page"}
+            return web.Response(text=json.dumps(response_obj), status=200)
+
+        cnt_pages = ceil(all_cities/cnt_cities_on_page)
+        end_of_previous_page = cnt_cities_on_page*(page-1)
+
+        result_page = []
+        if page <= cnt_pages and page != 0:
+            if end_of_previous_page + cnt_cities_on_page <= all_cities:
+                for i in range(cnt_cities_on_page):
+                    result_page.append(make_a_city_dict(cities_list[end_of_previous_page + i]))
+            else:
+                for i in range(all_cities - end_of_previous_page):
+                    result_page.append(make_a_city_dict(cities_list[end_of_previous_page + i]))
+        else:
+            response_obj = {'status': 'failed', 'message': "This page doesn't exist: "
+                                                           "something wrong with the number of page"}
+            return web.Response(text=json.dumps(response_obj), status=200)
+
+        response_obj = {'status': 'success', 'message': result_page}
+        return web.Response(text=json.dumps(response_obj, ensure_ascii=False), status=200)
+    except (ValueError, KeyError):
+        response_obj = {'status': 'failed', 'message': 'something wrong with input data'}
+        return web.Response(text=json.dumps(response_obj), status=200)
+
+
+async def comparison(request):
+    city1 = request.query('city1')
+    city2 = request.query('city2')
     pass
 
 
 app = web.Application()
 app.router.add_get('/api/v1/info_by_id', info_by_id)
 app.router.add_get('/api/v1/cities_page', cities_page)
-app.router.add_get('/', handle)
 
 web.run_app(app, port=8000)
