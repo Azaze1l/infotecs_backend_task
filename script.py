cce2707427bb47028de30987e61e63cabdf5860c
Azaze1l <56613711+Azaze1l@users.noreply.github.com>
2021-02-14 21:28:24 +0300

    final
    all is done

--- script.py
+++ script.py
@@ -1,17 +1,8 @@
 from aiohttp import web
 import json
 from math import ceil
-"""
-TODO:
-Реализованный сервер должен предоставлять REST API сервис со следующими методами:
-1.	Метод принимает идентификатор geonameid и возвращает информацию о городе.
-2.	Метод принимает страницу и количество отображаемых на странице городов и возвращает список городов с их информацией. 
-3.	Метод принимает названия двух городов (на русском языке) и получает информацию о найденных городах, а также 
-    дополнительно: какой из них расположен севернее и одинаковая ли у них временная зона (когда несколько городов имеют 
-    одно и то же название, разрешать неоднозначность выбирая город с большим населением; если население совпадает, 
-    брать первый попавшийся)
-"""
 
+# Данные из файла RU.txt сохраняются в список
 file = open('RU.txt', 'r', encoding='utf-8')
 cities_list = []
 for line in file:
@@ -19,6 +10,9 @@ for line in file:
 
 
 def make_a_city_dict(city):
+    """
+    Вспомогательная функция; создает словарь, содержащий свойства города.
+    """
     city_dict = {'geonameid': city[0], 'name': city[1],
                  'asciiname': city[2], 'alternatenames': city[3],
                  'latitude': city[4], 'longtitude': city[5],
@@ -38,10 +32,14 @@ async def info_by_id(request):
     """
     try:
         geonameid = int(request.query['id'])
+
+        # Если введенный клиентом geonameid больше максимального или меньше минимального geonameid, лежащего в
+        # базе данных, то сервер отправляет ответ со статусом 500
         if geonameid < int(cities_list[0][0]) or geonameid > int(cities_list[-1][0]):
             response_obj = {'status': 'failed', 'message': "Couldn't find any city by this id"}
             return web.Response(text=json.dumps(response_obj), status=500)
 
+        # нужный элемент находится посредством алгоритма бинарного поиска
         left_b = -1
         right_b = 201177
         while left_b < right_b - 1:
@@ -68,20 +66,27 @@ async def cities_page(request):
         cnt_cities_on_page = int(request.query['count'])
 
         all_cities = 201177
+
+        # Проводится проверка корректности введенных клиентом данных
         if cnt_cities_on_page > all_cities or cnt_cities_on_page == 0:
             response_obj = {'status': 'failed', 'message': "This page doesn't exist:"
                                                            " something wrong with counting cities on the page"}
             return web.Response(text=json.dumps(response_obj), status=200)
 
-        cnt_pages = ceil(all_cities/cnt_cities_on_page)
-        end_of_previous_page = cnt_cities_on_page*(page-1)
+        cnt_pages = ceil(all_cities / cnt_cities_on_page)
+
+        # номер последнего элемента страницы предыдущей искомой
+        end_of_previous_page = cnt_cities_on_page * (page - 1)  # (начало отсчеты индексируется единицей)
 
         result_page = []
+
+        # проводится проверка корректности введенных данных
         if page <= cnt_pages and page != 0:
             if end_of_previous_page + cnt_cities_on_page <= all_cities:
                 for i in range(cnt_cities_on_page):
                     result_page.append(make_a_city_dict(cities_list[end_of_previous_page + i]))
             else:
+                # исключительный случай обработки вывода последней страницы
                 for i in range(all_cities - end_of_previous_page):
                     result_page.append(make_a_city_dict(cities_list[end_of_previous_page + i]))
         else:
@@ -96,14 +101,59 @@ async def cities_page(request):
         return web.Response(text=json.dumps(response_obj), status=200)
 
 
+async def find_a_correct_city(name):
+    """
+    Вспомогательная функция; вовзращает город, найденный по альтернативному имени.
+    """
+    result = [i for i, city in enumerate(cities_list) if name in city[3].split(',')]
+    max_population = -1
+    cities_list_index = None
+    for i in result:
+        if int(cities_list[i][14]) > max_population:
+            max_population = int(cities_list[i][14])
+            cities_list_index = i
+    city1 = make_a_city_dict(cities_list[cities_list_index])
+    return city1
+
+
 async def comparison(request):
-    city1 = request.query('city1')
-    city2 = request.query('city2')
-    pass
+    """
+    Метод принимает названия двух городов (на русском языке) и получает информацию о найденных городах, а также
+    дополнительно: какой из них расположен севернее и одинаковая ли у них временная зона (когда несколько городов имеют
+    одно и то же название, разрешать неоднозначность выбирая город с большим населением; если население совпадает,
+    брать первый попавшийся)
+    """
+    try:
+        name1 = request.query['city1']
+        name2 = request.query['city2']
+        city1 = await find_a_correct_city(name1)
+        city2 = await find_a_correct_city(name2)
+
+        if city1.get('latitude') > city2.get('latitude'):
+            northern = 'city1'
+        else:
+            northern = 'city2'
+
+        if city1.get('timezone') == city2.get('timezone'):
+            same_timezone = True
+        else:
+            same_timezone = False
+        response_obj = {'status': 'success',
+                        'message': {'city1': city1, 'city2': city2, 'northern': northern,
+                                    'same_timezone': same_timezone}
+                        }
+        return web.Response(text=json.dumps(response_obj, ensure_ascii=False), status=200)
+    except Exception as e:
+
+        response_obj = {'status': 'failed', 'message': "something wrong with input data or city "
+                                                       "with this name doesn't exist in the database: {}".format(str(e))
+                        }
+        return web.Response(text=json.dumps(response_obj), status=200)
 
 
 app = web.Application()
 app.router.add_get('/api/v1/info_by_id', info_by_id)
 app.router.add_get('/api/v1/cities_page', cities_page)
+app.router.add_get('/api/v1/comparison', comparison)
 
 web.run_app(app, port=8000)
